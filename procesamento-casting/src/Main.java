1627fd956a9d7e5000164cd69084892603351b38
Henrique Gibi <henriquedepadua@yahoo.com.br>
2020-11-30 21:44:36 -0300

    Casting e Processamento

---
+++ procesamento-casting/src/Main.java
@@ -0,0 +1,46 @@
+import java.util.Locale;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		
+		/*
+		int x, y;
+		x = 5;
+		y = 2 * x;
+		System.out.println(x);
+		System.out.println(y);
+		
+		int x;
+		double y;
+		x = 5;
+		y = 2 * x;
+		System.out.println(x);
+		System.out.println(String.format("%.1f", y));
+		
+		double b1, b2, h, area;
+		b1 = 6.0;
+		b2 = 8.0;
+		h = 5.0;
+		area = (b1 + b2) / 2.0 * h;
+		System.out.println(area);
+		
+		int a, b;
+		double resultado;
+		a = 5;
+		b = 2;
+		resultado = (double) a / b;
+		System.out.println(resultado); 
+		*/
+		
+		double a;
+		int b;
+		a = 5.0;
+		b = (int) a;
+		System.out.println(b); 
+
+	}
+
+}
